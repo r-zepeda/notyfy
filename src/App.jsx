a6a3b01bdb43753f15647fb4493dc601f9da9b3b
r-zepeda <ralphpzepeda@gmail.com>
2024-02-17 12:23:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import './App.css';
-import './styles/main.css';
-import SideNav from './components/side-nav';
-import MainNav from './components/main-nav';
+import "./App.css";
+import "./styles/main.css";
+import SideNav from "./components/side-nav";
+import MainNav from "./components/main-nav";
 
 function App() {
   return (
     <div className="App">
-      <div className='dashboard'>
+      <div className="dashboard">
         <SideNav />
         <main>
           <MainNav />
-          <div className='main-body'>
-            <h1 className='main-greeting'>Hello, Ralph.</h1>
-            <h1 className='sub-greeting'>How can I help you today?</h1>
+          <div className="main-body">
+            <h1 className="main-greeting">Hello, Ralph.</h1>
+            <h1 className="sub-greeting">How may I help you today?</h1>
           </div>
         </main>
       </div>
-
     </div>
   );
 }
